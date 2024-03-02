d8ee49629c1d5b831ee1412b822ab538315633d4
anikapatychuk <paty1650@mylaurier.ca>
2024-03-01 19:06:51 -0500

    March 1 code added

--- mobile/voice_journal_app/lib/home.dart
+++ mobile/voice_journal_app/lib/home.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(
-    const MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        body: Align(
-          alignment:
-      Alignment.bottomCenter,
-          child: Icon(
-            Icons.add_circle_rounded,
-            color: Colors.red,
-            size: 100.0,
-          )
-        )
-      )
-    )
+        body: Container(), //
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround, // Spaces the icons evenly in the row
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.auto_graph),
+                onPressed: () {
+                  // Handles the button press
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  // Handles the button press
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.date_range),
+                onPressed: () {
+                  // Handles the button press
+                },
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Handles the button press
+          },
+          child: Icon(Icons.add_circle_rounded),
+          backgroundColor: const Color.fromARGB(255, 166, 101, 96),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, // Centers the button above the bar
+      ),
+    ),
   );
 }
