6a0ea64e48d18423310ef19bbe816e8e5d743bf0
liaorosemary <liaorosemary@gmail.com>
2024-03-07 17:56:09 -0500

    Quick update 2

--- api/recording_analysis/views.py
+++ api/recording_analysis/views.py
@@ -49,6 +49,13 @@ def transcribe_audio(request):
                  correct transcription:""" + result
 
     if (USING_OPENAI):
+        # Result:
+            """
+                {
+                    "transcript": "Even though we face the difficulties of today and tomorrow, I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up."
+                }
+            """
+
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
