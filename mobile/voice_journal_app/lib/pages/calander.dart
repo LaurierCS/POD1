336e9fabdb60772c0cba06d5deea7905825b4399
TaseskiCS <tony.works.biz@gmail.com>
2024-03-14 19:04:13 -0400

    added calander page

---
+++ mobile/voice_journal_app/lib/pages/calander.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; //library for date calculations, formatting, locales, etc...
+import './day_details.dart';
+
+void main() {
+  runApp(CalendarApp());
+}
+
+class CalendarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calendar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CalendarPage(),
+    );
+  }
+}
+
+class CalendarPage extends StatefulWidget {
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime _selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(252, 252, 252, 1),
+      // appBar: AppBar(
+      //   title: Text('Calendar'),
+      //   centerTitle: true,
+      // ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    setState(() {
+                      _selectedDate = DateTime(_selectedDate.year, _selectedDate.month - 1, 1);
+                    });
+                  },
+                ),
+                Text(
+                  DateFormat.yMMM().format(_selectedDate),
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    setState(() {
+                      _selectedDate = DateTime(_selectedDate.year, _selectedDate.month + 1, 1);
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 7,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+              ),
+              itemCount: DateTime(_selectedDate.year, _selectedDate.month + 1, 0).day,
+              itemBuilder: (BuildContext context, int index) {
+                DateTime day = DateTime(_selectedDate.year, _selectedDate.month, index + 1);
+                return GestureDetector(
+                  onTap: () {
+                    // Handle day selection
+                     Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DayDetailsPage(selectedDate: day),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: const Color.fromARGB(255, 109, 110, 110).withOpacity(0.5),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '${day.day}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ mobile/voice_journal_app/lib/pages/day_details.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class DayDetailsPage extends StatelessWidget {
+  final DateTime selectedDate;
+
+  DayDetailsPage({required this.selectedDate});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Day Details'),
+      ),
+      body: Column(
+        children: <Widget>[
+          // Display the selected date
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              '${selectedDate.year}-${selectedDate.month}-${selectedDate.day}',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          // Display hourly basis vertically
+          Expanded(
+            child: ListView.builder(
+              itemCount: 24,
+              itemBuilder: (BuildContext context, int index) {
+                String hour = '${index.toString().padLeft(2, '0')}:00';
+                return ListTile(
+                  title: Text(hour),
+                  // You can add more details or functionality here for each hour
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
