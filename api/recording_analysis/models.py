890eeb4053ec9002c55b87434cdd9bb2b00cee65
liaorosemary <liaorosemary@gmail.com>
2024-03-27 19:07:18 -0400

    Made entry_title and emotions fields blank=True

--- api/recording_analysis/models.py
+++ api/recording_analysis/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class UploadedAudio(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    entry_title = models.CharField(max_length=250, default="")
-    emotions = models.CharField(max_length=250, default="")
+    entry_title = models.CharField(max_length=250, default="", blank=True)
+    emotions = models.CharField(max_length=250, default="", blank=True)
     recording = models.FileField(upload_to="recordings")
     uploaded_on = models.DateTimeField(auto_now_add=True)   # NOTE: might not need
 
